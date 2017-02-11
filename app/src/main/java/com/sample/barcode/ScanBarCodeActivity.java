11355d4b8dbc66436c785839586164e5e0848073
Renan Willamy <renanwillamy2@gmail.com>
2017-02-11 17:05:09 -0200

    Canvas line animation implementted

--- app/src/main/java/com/sample/barcode/ScanBarCodeActivity.java
+++ app/src/main/java/com/sample/barcode/ScanBarCodeActivity.java
@@ -3,10 +3,12 @@ package com.sample.barcode;
 import android.Manifest;
 import android.content.Intent;
 import android.content.pm.PackageManager;
+import android.content.res.Configuration;
 import android.graphics.Canvas;
 import android.graphics.Color;
 import android.graphics.Paint;
 import android.graphics.PixelFormat;
+import android.graphics.PorterDuff;
 import android.graphics.RectF;
 import android.media.SoundPool;
 import android.os.Bundle;
@@ -27,16 +29,31 @@ import com.google.android.gms.vision.barcode.BarcodeDetector;
 import java.io.IOException;
 import java.util.ArrayList;
 
-public class ScanBarCodeActivity extends AppCompatActivity implements SurfaceHolder.Callback {
+public class ScanBarCodeActivity extends AppCompatActivity implements SurfaceHolder.Callback,
+        Runnable {
     private static final int MY_PERMISSIONS_REQUEST_CAMERA = 100;
     public static final String BARCODE = "barcode";
-    private static final String TAG = "ScanBArCodeActivity";
+    public double mSquareSize = 0.80;
     private SurfaceView mCameraPreview;
     private CameraSource mCameraSource;
     private BarcodeDetector mBarcodeDetector;
     private SurfaceHolder mHolderTransparent;
+    private float mLineHeight;
     private Paint mPaint;
     private boolean mRunning;
+    private Canvas mCanvas;
+    private float mScreenWidth;
+    private float mScreenHeight;
+    private RectF[] mDarkRect;
+    private SoundPool mSoundPool;
+    private int mSoundID;
+    private boolean mSoundLoaded;
+    private Thread mThread;
+    private boolean mReverse;
+    private float mX1;
+    private float mX2;
+    private float mY1;
+    private float mY2;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -46,53 +63,82 @@ public class ScanBarCodeActivity extends AppCompatActivity implements SurfaceHol
         mCameraPreview = (SurfaceView) findViewById(R.id.camera_preview);
         SurfaceView mTransparentView = (SurfaceView) findViewById(R.id.TransparentView);
 
-
         mHolderTransparent = mTransparentView.getHolder();
         mHolderTransparent.setFormat(PixelFormat.TRANSPARENT);
         mPaint = new Paint();
+        loadSound();
         createCameraSource();
     }
 
     private void drawTransparentBackground() {
-        final int TRANSPARENCY = 50;
-        if (mHolderTransparent.getSurface().isValid()) {
-            Canvas mCanvas = mHolderTransparent.lockCanvas();
-            float screenWidth = mCameraPreview.getWidth();
-            float screenHeight = mCameraPreview.getHeight();
-            int squareSize = (int) ((int) screenWidth * (0.8));
-            float x1, x2, y1, y2;
-            x1 = (screenWidth - squareSize) / 2;
-            x2 = x1 + squareSize;
-            y1 = (screenHeight - squareSize) / 2;
-            y2 = y1 + squareSize;
-            RectF[] dark = new RectF[]{
-                    new RectF(0, 0, screenWidth, y1), new RectF(0, y1, x1, y2), new RectF(x2, y1,
-                    screenWidth, y2), new RectF(0, y2, screenWidth, screenHeight)};
-            mPaint = new Paint();
-            mPaint.setColor(Color.GREEN);
-            mPaint.setAlpha((TRANSPARENCY));
-            for (RectF r : dark) {
-                mCanvas.drawRect(r, mPaint);
-            }
-            drawHorizontalLine(mCanvas);
+        final int TRANSPARENCY = 45;
+        if (mDarkRect == null) {
+            initDarkRect();
+        }
+        drawSquareBorders();
+        mPaint = new Paint();
+        mPaint.setColor(Color.WHITE);
+        mPaint.setAlpha((TRANSPARENCY));
+        for (RectF r : mDarkRect) {
+            mCanvas.drawRect(r, mPaint);
+        }
 
-            mHolderTransparent.unlockCanvasAndPost(mCanvas);
+    }
+
+    private void drawSquareBorders() {
+        mPaint.setColor(Color.WHITE);
+        mPaint.setStrokeWidth(2);
+        mPaint.setStyle(Paint.Style.STROKE);
+        mCanvas.drawRect(mX1, mY1, mX2, mY2, mPaint);
+    }
+
+    private void initDarkRect() {
+        int squareSize = (int) ((int) mScreenWidth * mSquareSize);
+        mX1 = (mScreenWidth - squareSize) / 2;
+        mX2 = mX1 + squareSize;
+        mY1 = (mScreenHeight - squareSize) / 2;
+        mY2 = mY1 + squareSize;
+        mDarkRect = new RectF[]{
+                new RectF(0, 0, mScreenWidth, mY1), new RectF(0, mY1, mX1, mY2), new RectF(mX2, mY1,
+                mScreenWidth, mY2), new RectF(0, mY2, mScreenWidth, mScreenHeight)};
+    }
+
+    private void drawHorizontalLine() {
+        if (mLineHeight <= mY1) {
+            if (mReverse) {
+                mReverse = false;
+            } else {
+                mLineHeight = mY1;
+            }
+        } else if (mLineHeight >= mY2) {
+            if (!mReverse) mReverse = true;
+        }
+        mPaint = new Paint();
+        mPaint.setStyle(Paint.Style.STROKE);
+        mPaint.setColor(Color.GREEN);
+        mPaint.setStrokeWidth(5);
+        mCanvas.drawLine(mX1, mLineHeight, mX2, mLineHeight, mPaint);
+        if (mReverse) {
+            mLineHeight -= 7;
+        } else {
+            mLineHeight += 7;
         }
     }
 
-    private void drawHorizontalLine(Canvas mCanvas) {
-        float screenWidth = mCameraPreview.getWidth();
-        float screenHeight = mCameraPreview.getHeight();
-        int squareSize = (int) ((int) screenWidth * (0.8));
-        float x1, x2, y1, y2;
-        x1 = (screenWidth - squareSize) / 2;
-        x2 = x1 + squareSize;
+    private void drawCrossLine() {
+        int squareSize = (int) ((int) mScreenWidth * mSquareSize);
+        float y1, x1, x2;
+        x1 = mX1 + (squareSize / 2);
         mPaint = new Paint();
-        mPaint.setStyle(Paint.Style.STROKE);
         mPaint.setColor(Color.WHITE);
         mPaint.setStrokeWidth(5);
-        int height = mCanvas.getHeight() / 2;
-        mCanvas.drawLine(x1, height, x2, height, mPaint);
+        mCanvas.drawLine(x1, mY1 + 20, x1, mY1 - 50, mPaint);
+        mCanvas.drawLine(x1, mY2 + 50, x1, mY2 - 20, mPaint);
+        y1 = (mY1 + (squareSize) / 2);
+        x1 = (mScreenWidth - squareSize) / 2;
+        x2 = mX1 + squareSize;
+        mCanvas.drawLine(x1 - 50, y1, x1 + 20, y1, mPaint);
+        mCanvas.drawLine(x2 - 20, y1, x2 + 50, y1, mPaint);
     }
 
     private void createCameraSource() {
@@ -101,26 +147,7 @@ public class ScanBarCodeActivity extends AppCompatActivity implements SurfaceHol
                 .setAutoFocusEnabled(true)
                 .setRequestedPreviewSize(1600, 1024)
                 .build();
-        mCameraPreview.getHolder().addCallback(new SurfaceHolder.Callback() {
-            @Override
-            public void surfaceCreated(SurfaceHolder surfaceHolder) {
-                mRunning = true;
-            /*    Thread thread = new Thread(ScanBarCodeActivity.this);
-               // thread.start();*/
-           //     drawTransparentBackground();
-                startBarcodeDetection();
-            }
-
-            @Override
-            public void surfaceChanged(SurfaceHolder surfaceHolder, int i, int i1, int i2) {
-
-            }
-
-            @Override
-            public void surfaceDestroyed(SurfaceHolder surfaceHolder) {
-                mCameraSource.stop();
-            }
-        });
+        mCameraPreview.getHolder().addCallback(this);
     }
 
     public void startBarcodeDetection() {
@@ -141,31 +168,37 @@ public class ScanBarCodeActivity extends AppCompatActivity implements SurfaceHol
                     }
                     intent.putExtra(BARCODE, list);
                     setResult(CommonStatusCodes.SUCCESS, intent);
-                    // Load the sounds
-                    SoundPool soundPool = new SoundPool.Builder()
-                            .setMaxStreams(10)
-                            .build();
-                    final int soundID = soundPool.load(ScanBarCodeActivity.this, R.raw.beep, 1);
-                    soundPool.setOnLoadCompleteListener(new SoundPool.OnLoadCompleteListener() {
-                        @Override
-                        public void onLoadComplete(SoundPool soundPool, int sampleId, int status) {
-                            soundPool.play(soundID, 0.5f, 0.5f, 1, 0, 1);
-                            finish();
-                        }
-                    });
+                    playSound();
+                    finish();
                 }
             }
         });
     }
 
-    private void startCameraSource() {
-        if (ActivityCompat.checkSelfPermission(ScanBarCodeActivity.this
-                , Manifest.permission.CAMERA)
-                != PackageManager.PERMISSION_GRANTED) {
+    private void loadSound() {
+        mSoundPool = new SoundPool.Builder()
+                .setMaxStreams(10)
+                .build();
+        mSoundID = mSoundPool.load(ScanBarCodeActivity.this, R.raw.beep, 1);
+        mSoundPool.setOnLoadCompleteListener(new SoundPool.OnLoadCompleteListener() {
+            @Override
+            public void onLoadComplete(SoundPool soundPool, int sampleId, int status) {
+                mSoundLoaded = true;
+            }
+        });
+    }
 
+    private void playSound() {
+        if (mSoundLoaded) {
+            mSoundPool.play(mSoundID, 0.5f, 0.5f, 1, 0, 0);
+        }
+    }
+
+    private void startCameraSource() {
+        if (ActivityCompat.checkSelfPermission(ScanBarCodeActivity.this,
+                Manifest.permission.CAMERA) != PackageManager.PERMISSION_GRANTED) {
             ActivityCompat.requestPermissions(ScanBarCodeActivity.this,
-                    new String[]{Manifest.permission.CAMERA},
-                    MY_PERMISSIONS_REQUEST_CAMERA);
+                    new String[]{Manifest.permission.CAMERA}, MY_PERMISSIONS_REQUEST_CAMERA);
             return;
         }
         try {
@@ -188,33 +221,52 @@ public class ScanBarCodeActivity extends AppCompatActivity implements SurfaceHol
 
     @Override
     public void surfaceCreated(SurfaceHolder holder) {
-        tryDrawing(holder);
+        startCameraSource();
+        startThread();
+        startBarcodeDetection();
+    }
+
+    private void startThread() {
+        mRunning = true;
+        if (getResources().getConfiguration().orientation == Configuration.ORIENTATION_LANDSCAPE) {
+            mSquareSize = 0.45;
+        } else {
+            mSquareSize = 0.8;
+        }
+        mScreenWidth = mCameraPreview.getWidth();
+        mScreenHeight = mCameraPreview.getHeight();
+        mThread = new Thread(ScanBarCodeActivity.this);
+        mThread.start();
     }
 
     @Override
     public void surfaceChanged(SurfaceHolder holder, int frmt, int w, int h) {
-        tryDrawing(holder);
+        Log.d("SurfaceTest", "Resume");
     }
 
     @Override
     public void surfaceDestroyed(SurfaceHolder holder) {
-    }
-
-    private void tryDrawing(SurfaceHolder holder) {
-        Log.i(TAG, "Trying to draw...");
-
-        Canvas canvas = holder.lockCanvas();
-        if (canvas == null) {
-            Log.e(TAG, "Cannot draw onto the mCanvas as it's null");
-        } else {
-            drawMyStuff(canvas);
-            holder.unlockCanvasAndPost(canvas);
+        mCameraSource.stop();
+        boolean retry = true;
+        while (retry) {
+            try {
+                mThread.join();
+                retry = false;
+            } catch (InterruptedException e) {
+                // we will try it again and again...
+            }
         }
     }
 
-    private void drawMyStuff(final Canvas canvas) {
-        Log.i(TAG, "Drawing...");
-        canvas.drawRGB(255, 128, 128);
+    @Override
+    protected void onResume() {
+        super.onResume();
+        if (mThread != null) {
+            if (!mThread.isAlive()) {
+                startThread();
+            }
+        }
+
     }
 
     @Override
@@ -223,16 +275,23 @@ public class ScanBarCodeActivity extends AppCompatActivity implements SurfaceHol
         super.onPause();
     }
 
-    /*@Override
+    @Override
     public void run() {
         while (mRunning) {
+            if (!mHolderTransparent.getSurface().isValid()) continue;
+            mCanvas = mHolderTransparent.lockCanvas();
+            mCanvas.drawColor(Color.TRANSPARENT, PorterDuff.Mode.CLEAR);
 
-           // drawTransparentBackground();
+            drawHorizontalLine();
+            drawCrossLine();
+            drawTransparentBackground();
+
+            mHolderTransparent.unlockCanvasAndPost(mCanvas);
             try {
-                Thread.sleep(10000);
+                Thread.sleep(5);
             } catch (InterruptedException e) {
                 e.printStackTrace();
             }
         }
-    }*/
+    }
 }
